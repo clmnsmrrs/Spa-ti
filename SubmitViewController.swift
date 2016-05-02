120cdeb25e09758083c923c05cbe064fef1466ca
clmnsmrrs <clemensmorris@Clemenss-MacBook-Air.local>
2016-05-02 13:22:42 +0200

    2nd May Uni
    made the submit page

---
+++ SubmitViewController.swift
@@ -0,0 +1,83 @@
+//
+//  SubmitViewController.swift
+//  Späti
+//
+//  Created by Clemens Morris on 02/05/16.
+//  Copyright © 2016 Clemens Morris. All rights reserved.
+//
+
+import UIKit
+
+class SubmitViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    var name = "no name given"
+    var address = "no name given"
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var locationField: UITextField!
+    
+//    @IBAction func currentLocation(sender: AnyObject) {
+//        let locationManage = CLLocationManager()
+//        let authorizationStatus = CLLocationManager.authorizationStatus()
+//        
+//        if (authorizationStatus == .AuthorizedWhenInUse){
+//        location = locationManage.location!
+//        }
+//        else {
+//            
+//            let ac = UIAlertController(title: "Location sharing not authorized", message: "Please go to settings and share your location", preferredStyle: .Alert)
+//            ac.addAction(UIAlertAction(title: "Done", style: .Default, handler: nil))
+//            presentViewController(ac, animated: true, completion: nil)
+//            
+//        }
+//    }
+    
+    @IBAction func submitSpäti(sender: AnyObject) {
+        
+        if(nameField.text != "" && locationField.text != ""){
+            name = nameField.text!
+            address = locationField.text!
+        
+            let location = PFObject(className: "SubmitSpati")
+            location.setObject(name, forKey: "Name")
+            location.setObject(address, forKey: "Address")
+            location.saveInBackgroundWithBlock { (succeeded, error) -> Void in
+                if succeeded {
+                    let ac = UIAlertController(title: "Thanks for Sharing!", message: "You're making this app even more useful!", preferredStyle: .Alert)
+                    ac.addAction(UIAlertAction(title: "Go back to being awesome", style: .Default, handler: nil))
+                    self.presentViewController(ac, animated: true, completion: nil)
+                } else {
+                    print("Error: \(error) \(error!.userInfo)")
+                }
+            }
+            
+        }
+        else {
+            
+            let ac = UIAlertController(title: "Information Missing", message: "Please Add details to all fields", preferredStyle: .Alert)
+            ac.addAction(UIAlertAction(title: "Done", style: .Default, handler: nil))
+            presentViewController(ac, animated: true, completion: nil)
+            
+        }
+    }
+    
+        
+        
+    }
+    
+    
+    
+
